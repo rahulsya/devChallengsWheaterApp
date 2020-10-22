7e5f336b36849d90b8df73907afa669b33e43acb
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-10-23 00:27:32 +0700

    add converter to fahrenheit

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 import icons from '../MainContent/wheaterIcons'
 import dateFormat from '../../utils/dateFormat'
+import celciusToFahren from '../../utils/celciusToFahren'
 
-export default function SideBar({wheater,location}) {
+export default function SideBar({wheater,location,typeTemp}) {
     let icon=icons.find(icon=>icon.name===wheater.weather_state_name)
     return (
         <div className="container mx-auto px-5">
-            <div className="grid grid-cols gap-4 md:gap-12 lg:gap-32 place-content-center text-center py-5  text-white ">
-                <img src={icon.img_url} alt="clear" className="mt-5"/>
-                <div className="text-6xl mt-5">
-                    {wheater.the_temp.toFixed(0)} °c
+            <div className="grid grid-cols place-content-center lg:place-content-around text-center py-5 text-white h-600 lg:h-900" >
+                <img src={icon.img_url} alt="clear"/>
+                <div className="text-6xl">
+                    {typeTemp==='f' 
+                        ? `${celciusToFahren(wheater.the_temp.toFixed(0))} °F` 
+                        : `${wheater.the_temp.toFixed(0)} °C`
+                    }
                 </div>
-                <div className="text-2xl mt-8">
+                <div className="text-2xl">
                     {wheater.weather_state_name}
                 </div>
                 <div className="flex flex-col items-center text-1xl font-light capitalize text-white">
